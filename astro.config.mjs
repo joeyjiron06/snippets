13d3d6532145b69583b790b506f96533ba533d7e
joeyjiron06 <joeyjiron06@gmail.com>
2024-10-31 17:28:19 -0700

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { URL } from 'node:url'
+import GitHubDarkDefaultTheme from 'tm-themes/themes/github-dark-default.json'
 
+
+// GitHubDarkDefaultTheme.colors['editor.background'] = 'black'
+GitHubDarkDefaultTheme.colors['editor.background'] = '#0a0a0a'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +18,14 @@ export default defineConfig({
         '~': new URL('./src', import.meta.url).toString(),
       },
     },
+  },
+  markdown: {
+    shikiConfig: {
+      // themes:{'my-theme':GitHubDarkDefaultTheme},
+      theme: GitHubDarkDefaultTheme
+    },
+
+
   },
   integrations: [
     icon(),
