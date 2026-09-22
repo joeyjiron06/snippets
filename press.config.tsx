9ffbd599222031b0cdabe1b9fe07f9267d88ce8e
Joey Jiron <joeyjiron@microsoft.com>
2026-09-22 15:59:27 -0700

    fixed favicon extension issue

--- press.config.tsx
+++ press.config.tsx
@@ -57,7 +57,7 @@ const config = defineConfig({
             href="https://fonts.googleapis.com/css2?family=Geist:ital,wght@0,100..900;1,100..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/snippets/favicon.ico" type="image/svg+xml" />
+          <link rel="icon" href="/snippets/favicon.svg" type="image/svg+xml" />
         </>
       );
     },
